afbf12d9d8050f899e69cb2914d2050767d712cb
Markus Witt <feuerrot@feuerrot.org>
2014-03-06 15:43:23 +0100

    Download youtube-dl and bottle.py if not already available

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+import os
+import urllib.request
 
+download = {
+		'bottle.py' : 'https://raw.github.com/defnull/bottle/d7780fd5d5c2baeb0152864138adbbdfcf83e6e4/bottle.py',
+		'youtube-dl' : 'https://yt-dl.org/downloads/2014.03.04.2/youtube-dl'
+}
+
+for elem in download:
+	if not os.path.isfile(elem):
+		urllib.request.urlretrieve(download[elem], elem)
